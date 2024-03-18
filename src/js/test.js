ec76d937ae4cbd0e9689acc58382337c5b037495
Andr3wM1ll3r <an873997@ucf.edu>
2024-03-18 02:30:27 -0400

    some formatting

--- src/js/test.js
+++ src/js/test.js
@@ -6,15 +6,16 @@ window.$ = window.jQuery = require('../node_modules/jquery/dist/jquery.min.js');
 //     ipcRenderer.send("receive")
 // })
 
-$(".collapsible").find(".left").find("button").on("click", (event) => {
-    let target = $(event.target)
-    let hidden = target.closest(".collapsible").find(".hidden")
-    
-    if (target.css("transform") == "matrix(-1, 0, 0, -1, 0, 0)") {
-        target.css("transform", "rotate(270deg)")
+$(".collapsible").on("click", (event) => {
+    let collapsible = $(event.target).closest(".collapsible")
+    let hidden = collapsible.find(".hidden")
+    let svg = collapsible.find(".left").find("span")
+
+    if (svg.css("transform") == "matrix(-1, 0, 0, -1, 0, 0)") {
+        svg.css("transform", "rotate(270deg)")
         hidden.css("display", "block")
     } else {
-        target.css("transform", "rotate(180deg)")
+        svg.css("transform", "rotate(180deg)")
         hidden.css("display", "none")
     }
 })
