972d8322437c6e728f737ba97a3f31854fbed478
Andr3wM1ll3r <an873997@ucf.edu>
2024-03-18 02:00:20 -0400

    collapsible menu

--- src/js/test.js
+++ src/js/test.js
@@ -1,7 +1,20 @@
-const { ipcRenderer } = require("electron");
+// const { ipcRenderer } = require("electron");
 
 window.$ = window.jQuery = require('../node_modules/jquery/dist/jquery.min.js');
 
-ipcRenderer.on("alert", () => {
-    ipcRenderer.send("receive")
+// ipcRenderer.on("alert", () => {
+//     ipcRenderer.send("receive")
+// })
+
+$(".collapsible").find(".left").find("button").on("click", (event) => {
+    let target = $(event.target)
+    let hidden = target.closest(".collapsible").find(".hidden")
+    
+    if (target.css("transform") == "matrix(-1, 0, 0, -1, 0, 0)") {
+        target.css("transform", "rotate(270deg)")
+        hidden.css("display", "block")
+    } else {
+        target.css("transform", "rotate(180deg)")
+        hidden.css("display", "none")
+    }
 })
